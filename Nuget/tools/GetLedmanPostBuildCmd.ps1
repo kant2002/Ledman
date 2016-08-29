a698f6c2bd490c3578887d803e18af645584870d
Andrey Kurdyumov <kant2002@gmail.com>
2016-08-29 18:54:36 +0600

    Make Nuget package which works for regular .NET projects and which is copy native dependencies. This package does not have support for the .NET Core projects

---
+++ Nuget/tools/GetLedmanPostBuildCmd.ps1
@@ -0,0 +1,8 @@
+﻿$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
+$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
+
+$NativeAssembliesDir = Join-Path $path "NativeBinaries"
+
+$LedmanPostBuildCmd = "
+xcopy /s /y `"$NativeAssembliesDir`" `"`$(TargetDir)`"
+"
---
+++ Nuget/tools/install.ps1
@@ -0,0 +1,11 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+. (Join-Path $toolsPath "GetLedmanPostBuildCmd.ps1")
+
+# Get the current Post Build Event cmd
+$currentPostBuildCmd = $project.Properties.Item("PostBuildEvent").Value
+
+# Append our post build command if it's not already there
+if (!$currentPostBuildCmd.Contains($LedmanPostBuildCmd)) {
+    $project.Properties.Item("PostBuildEvent").Value += $LedmanPostBuildCmd
+}
