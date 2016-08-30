5381c6bd1b22f4a51b92c03f80ed832b60b11b59
Andrey Kurdyumov <kant2002@gmail.com>
2016-08-30 11:33:00 +0600

    Add support for .NET Core but without native modules

--- Nuget/tools/GetLedmanPostBuildCmd.ps1
+++ Nuget/tools/GetLedmanPostBuildCmd.ps1
@@ -1,7 +1,7 @@
 ﻿$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
 $path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
 
-$NativeAssembliesDir = Join-Path $path "NativeBinaries"
+$NativeAssembliesDir = Join-Path $path "runtimes\win7"
 
 $LedmanPostBuildCmd = "
 xcopy /s /y `"$NativeAssembliesDir`" `"`$(TargetDir)`"
